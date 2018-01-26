d0ed9b103814c8500328fe8e3bfc1081c1f5150d
etselamet <eselamet@etu.edu.tr>
2018-01-27 01:34:44 +0300

    login firebase entegration

--- SpeculAR/app/src/main/java/com/mate/specular/MainActivity.java
+++ SpeculAR/app/src/main/java/com/mate/specular/MainActivity.java
@@ -1,20 +1,34 @@
 package com.mate.specular;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     Button signUpTextButton;
+    Button loginButton;
+    protected EditText username;
+    protected EditText pass;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
     static {
         if(OpenCVLoader.initDebug()){
             Log.d(TAG, "OpenCV succesfully loaded.");
@@ -34,7 +48,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
         signUpTextButton = (Button) findViewById(R.id.signUpButton);
+        username = findViewById(R.id.userLoginEmail);
+        pass = (EditText) findViewById(R.id.userLoginPassword);
+        loginButton = (Button) findViewById(R.id.loginButton);
+
         signUpTextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,26 +65,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = username.getText().toString();
+                String password = pass.getText().toString();
+
+                email = email.trim();
+                password = password.trim();
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage(R.string.login_error_message)
+                            .setTitle(R.string.login_error_title)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+                    mFirebaseAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                       /* Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);*/
+                                    } else {
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                                        builder.setMessage(task.getException().getMessage())
+                                                .setTitle(R.string.login_error_title)
+                                                .setPositiveButton(android.R.string.ok, null);
+                                        AlertDialog dialog = builder.create();
+                                        dialog.show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
     }
-
-    /*public EditText username =  findViewById(R.id.userLoginEmail);
-    public EditText password = (EditText) findViewById(R.id.userLoginPassword);
-    public void loginButtonAction(View view){
-        Log.i("Info", "Login Button Action method started");
-
-        Log.i("Info", "Username: " + username.getText().toString() + " password: " + password.getText().toString());
-
-        //TODO login servisi icin gerekli islem gelecek
-
-    }*/
-
-    public void signupButtonAction(){
-        Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
-        startActivity(intent);
-    }
-
-
-
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
