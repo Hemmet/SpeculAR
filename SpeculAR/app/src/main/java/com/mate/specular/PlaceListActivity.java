ea3a9e1e962565f3cbb6d5326a94ef88d8bfd0aa
etselamet <eselamet@etu.edu.tr>
2018-03-01 22:27:21 +0300

    new content camera screen

--- SpeculAR/app/src/main/java/com/mate/specular/PlaceListActivity.java
+++ SpeculAR/app/src/main/java/com/mate/specular/PlaceListActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -86,5 +89,25 @@ public class PlaceListActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.place_list_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addNewContent:
+                startActivity(new Intent(PlaceListActivity.this, NewContentCameraActivity.class));
+                break;
+            default:
+                break;
+        }
+
+        return true;
     }
 }
