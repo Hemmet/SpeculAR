1319b8b09618e3fac3d827cb0536c84c708f791d
hacetin <h.cetin@etu.edu.tr>
2018-03-20 01:57:58 +0300

    updated create new model

--- SpeculAR/app/src/main/java/com/mate/specular/CreateModelActivity.java
+++ SpeculAR/app/src/main/java/com/mate/specular/CreateModelActivity.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.mate.specular.model.InfoButton;
+import com.mate.specular.model.QuizButton;
 
 import java.util.ArrayList;
 
@@ -22,6 +23,7 @@ public class CreateModelActivity extends Activity {
     ImageView backgroundImageView;
     RelativeLayout relativeLayout;
     ArrayList<InfoButton> newInfoTagList;
+    ArrayList<QuizButton> newQuizTagList;
     Context context;
     DisplayMetrics metrics;
 
@@ -35,6 +37,7 @@ public class CreateModelActivity extends Activity {
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
         newInfoTagList = new ArrayList<InfoButton>();
+        newQuizTagList = new ArrayList<QuizButton>();
         relativeLayout = (RelativeLayout)findViewById(R.id.createModelLayout);
         backgroundImageView = (ImageView) findViewById(R.id.backgroundImageView);
         putImage();
@@ -46,6 +49,25 @@ public class CreateModelActivity extends Activity {
                 InfoButton newInfoTag = createInfoTag();
                 newInfoTagList.add(newInfoTag);
                 relativeLayout.addView(newInfoTag);
+            }
+        });
+
+        ImageButton addQuizTag = (ImageButton) findViewById(R.id.addQuizTagImageButton);
+        addQuizTag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                QuizButton newQuizTag = createQuizTag();
+                newQuizTagList.add(newQuizTag);
+                relativeLayout.addView(newQuizTag);
+            }
+        });
+
+        ImageButton okImageButton = (ImageButton) findViewById(R.id.addOkImageButton);
+        okImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TO DO
+                //Tag ler database e kaydedilecek
             }
         });
     }
@@ -75,29 +97,34 @@ public class CreateModelActivity extends Activity {
         return newInfoTag;
     }
 
-    private InfoButton createInTag() {
-        final InfoButton newInfoTag =  new InfoButton(context, "Put info here", "Put header here");
-        newInfoTag.setY(metrics.heightPixels / 2);
-        newInfoTag.setX(metrics.widthPixels / 2);
-        newInfoTag.setImageResource(R.drawable.info_tag_black);
-        newInfoTag.setBackgroundColor(Color.TRANSPARENT);
-        newInfoTag.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+    private QuizButton createQuizTag() {
+        ArrayList<String> options = new ArrayList<String>();
+        options.add("Option 1");
+        options.add("Option 2");
+        options.add("Option 3");
+        options.add("Option 4");
+        final QuizButton newQuizTag =  new QuizButton(context, options,"Put question here");
+        newQuizTag.setY(metrics.heightPixels / 2);
+        newQuizTag.setX(metrics.widthPixels / 2);
+        newQuizTag.setImageResource(R.drawable.quiz_tag_black);
+        newQuizTag.setBackgroundColor(Color.TRANSPARENT);
+        newQuizTag.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
 
-        newInfoTag.setOnTouchListener(new View.OnTouchListener() {
+        newQuizTag.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 switch (motionEvent.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                     case MotionEvent.ACTION_MOVE:
                     case MotionEvent.ACTION_UP:
-                        newInfoTag.setX(motionEvent.getRawX());
-                        newInfoTag.setY(motionEvent.getRawY());
+                        newQuizTag.setX(motionEvent.getRawX());
+                        newQuizTag.setY(motionEvent.getRawY());
                 }
                 return true;
             }
         });
 
-        return newInfoTag;
+        return newQuizTag;
     }
 
     public void putImage(){
