04346143ac7db1946b38ed45a9d7f595e0fe1896
M Enes Turgut <met914@gmail.com>
2018-03-20 01:57:13 +0300

    Runtime camera permission added

--- SpeculAR/app/src/main/java/com/mate/specular/MainActivity.java
+++ SpeculAR/app/src/main/java/com/mate/specular/MainActivity.java
@@ -1,11 +1,18 @@
 package com.mate.specular;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -16,12 +23,14 @@ import org.opencv.android.OpenCVLoader;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static final int PERMISSION_REQUEST_CAMERA = 100;
     Button signUpTextButton;
     Button loginButton;
     protected EditText username;
     protected EditText pass;
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
+    private View mLayout;
 
     static {
         if(OpenCVLoader.initDebug()){
@@ -41,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLayout = findViewById(R.id.main_layout);
 
         MockDB.initializeMockDB();
 
@@ -48,13 +58,7 @@ public class MainActivity extends AppCompatActivity {
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
 
-        if (mFirebaseUser == null) {
-            // Not logged in, launch the Log In activity
-            loadLogInView();
-        }
-        else {
-            loadPlaceList();
-        }
+        showCameraPreview();
     }
 
     private void loadLogInView() {
@@ -76,4 +80,65 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    private void showCameraPreview() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            Snackbar.make(mLayout,
+                    R.string.camera_permission_available,
+                    Snackbar.LENGTH_SHORT).show();
+            if (mFirebaseUser == null) {
+                // Not logged in, launch the Log In activity
+                loadLogInView();
+            }
+            else {
+                loadPlaceList();
+            }
+        } else {
+            requestCameraPermission();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Snackbar.make(mLayout, R.string.camera_permission_granted,
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+                if (mFirebaseUser == null) {
+                    // Not logged in, launch the Log In activity
+                    loadLogInView();
+                }
+                else {
+                    loadPlaceList();
+                }
+            } else {
+                Snackbar.make(mLayout, R.string.camera_permission_denied,
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+    private void requestCameraPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                android.Manifest.permission.CAMERA)) {
+            Snackbar.make(mLayout, R.string.camera_access_required,
+                    Snackbar.LENGTH_INDEFINITE).setAction(R.string.ok, new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Request the permission
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{android.Manifest.permission.CAMERA},
+                            PERMISSION_REQUEST_CAMERA);
+                }
+            }).show();
+
+        } else {
+            Snackbar.make(mLayout, R.string.camera_unavailable, Snackbar.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+        }
+    }
 }
